0b40099ed99028a6bc27f73cbeba02fcb8b86ead
Jarek700 <60602091+Jarek700@users.noreply.github.com>
2020-02-24 15:16:37 +0100

    Add files via upload

--- ZadaniePiramida/ZadaniePiramida/Program.cs
+++ ZadaniePiramida/ZadaniePiramida/Program.cs
@@ -8,30 +8,57 @@ namespace ZadaniePiramida
         {
             //Twoim zadaniem jest stworzenie aplikacji konsolowej, która wyświetli napis :
             //„Podaj wysokość”
-            //Poczym narysuje piramide z gwiazdek.
+            //Poczym narysuje piramide z gwiazdek. Dla wartości ujemnych do góry nogami
 
             Console.Write("Podaj wysokość");
             String yw = Console.ReadLine();
 
+            Console.Write("Podaj znak do budowy:");
+            String zx = Console.ReadLine();
+
+            char znak = char.Parse(zx);
             int wysokosc = int.Parse(yw);
 
-
-            for (int i = 0; i < wysokosc; i++)             
-            {
-                for (int j = (wysokosc-i)/2; j > 0; j--)         
+            if (wysokosc >= 0)
+            {           
+                for (int i = 0; i < wysokosc; i++)
                 {
-                    Console.Write(" ");
-                }
-                for (int k = 0; k <= i; k++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
+                    for (int j = (wysokosc - i) / 2; j > 0; j--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int k = 0; k <= i; k++)
+                    {
+                        Console.Write(znak);
+                       
+                    }
+                    Console.WriteLine();
 
+                }
             }
+            else
+            {
+                
+                for (int i = (wysokosc * -1) - 1; i >=0; i--)
 
+                {
 
+                        {
+                            for (int j = ((wysokosc * -1) - i) / 2; j > 0; j--)
+                            {
+                                Console.Write(" ");
+                            }
+                            for (int k = 0; k <= i; k++)
+                            {
+                                Console.Write(znak);
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
+            }
             Console.ReadLine();
+
         }
     }
 }
