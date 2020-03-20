619f20b2d43fac24c2c3fc02dea5e71a9d67d9e9
Jarek700 <60602091+Jarek700@users.noreply.github.com>
2020-03-20 15:16:44 +0100

    Add files via upload

---
+++ RokPrzestepny/RokPrzestepny/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RokPrzestepny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Podaj Rok(yyyy):");
+            int Rok = int.Parse(Console.ReadLine());
+
+
+            if (CzyRokPrzestepny(Rok))
+            {
+                Console.WriteLine("Rok Przestepny");
+            }
+           
+            else
+            {
+                Console.WriteLine("Rok zwykły");
+            }
+
+
+            
+        }
+        static bool CzyRokPrzestepny(int Rok)
+        {
+            return ((Rok % 4 == 0) && (Rok % 100 != 0)) || (Rok % 400 == 0);
+        }
+    }
+}
