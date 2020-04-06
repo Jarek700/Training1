b27c63d75d1f55d8b2eee0bcc820053716c19410
Jarek700 <60602091+Jarek700@users.noreply.github.com>
2020-04-06 15:36:03 +0200

    Add files via upload

---
+++ liczbapierwsza/liczbapierwsza/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace liczbapierwsza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj Liczbę:");
+            String liczba = Console.ReadLine();
+            int wartosc = int.Parse(liczba);
+
+
+            bool czypierwsza = Czyliczbapierwsza(wartosc);
+
+
+            if (czypierwsza)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(liczba+": to jest liczba pierwsza !!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(liczba + ": to NIE jest liczba pierwsza !!");
+            }
+
+            
+            Console.ReadLine();
+        }
+        static bool Czyliczbapierwsza(int L)
+        {
+            for (int i = 2; i < L; i++)
+            {
+                if (L % i == 0)
+                {
+                    return false;
+                }
+                
+              
+            }
+                   return true;
+            }
+
+    }
+}
